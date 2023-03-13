f242874eaa8253720c64e24013eb958a5585730a
yankeeboomer <dale.c.vincent@gmail.com>
2023-03-13 00:50:07 -0500

    Added button to destroy window. Added comments for what program is doing for reference. 3/13/23 0050

--- Window Test.py
+++ Window Test.py
@@ -1,4 +1,30 @@
-import tkinter as tk
+import tkinter
+from tkinter import *
+from PIL import Image, ImageTk
 
-window = tk.Tk()
+# Create a window
+window = Tk()
+
+# Size window
+window.geometry("500x500")
+
+# Name a window
+window.title("Dice Bag")
+
+# create a button to destroy window
+btn = Button(window, text='Click Me!', bd='5', command=window.destroy)
+
+# set position of button to top of window
+btn.pack(side='top')
+
+# Create a photo image object of the image in the path
+image1 = Image.open("d20.png")
+test = ImageTk.PhotoImage(image1)
+
+label1 = tkinter.Label(image=test)
+label1.image = test
+
+# Position Image
+label1.place(x=120, y=75)
+
 window.mainloop()
